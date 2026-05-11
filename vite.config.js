b7186bad3a7af068673ad24cc7e1198c351d926d
bbbbbbaby <165198536+bbbbbbaby@users.noreply.github.com>
2026-05-11 13:29:44 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,37 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['icon-192x192.png', 'icon-512x512.png'],
-      manifest: {
-        name: 'Per-You Garden',
-        short_name: 'PUG',
-        description: 'Il gioco del Per-You Garden',
-        theme_color: '#1a3590',
-        background_color: '#1e1060',
-        display: 'standalone',
-        orientation: 'portrait',
-        scope: '/',
-        start_url: '/',
-        icons: [
-          {
-            src: 'icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable'
-          }
-        ]
-      }
-    })
-  ]
+  plugins: [react()]
 })
